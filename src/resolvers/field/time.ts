36bfe4aab8178d143aeb1859af6c9321a53108b5
Lukas Romsicki <lukas@romsicki.com>
2019-08-05 13:14:10 -0400

    Find UTC time entry specifically

--- src/resolvers/field/time.ts
+++ src/resolvers/field/time.ts
@@ -2,7 +2,9 @@ import moment from 'moment';
 
 export default function time(obj: any) {
   const { time } = obj;
-  const utcTimeObject = time[0];
+  const utcTimeObject = time.find(
+    dateTimeObject => dateTimeObject.zone === 'UTC'
+  );
 
   const date = moment(utcTimeObject.timeStamp, 'YYYYMMDDHHmmss');
 
