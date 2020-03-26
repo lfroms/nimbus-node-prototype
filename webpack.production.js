74743435cd3d702e022e856969051615df8bb8b9
Lukas Romsicki <lukas@romsicki.com>
2020-03-26 13:00:12 -0400

    Add missing line

--- webpack.production.js
+++ webpack.production.js
@@ -3,6 +3,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const merge = require('webpack-merge');
 const nodeExternals = require('webpack-node-externals');
 const path = require('path');
+const webpack = require('webpack');
 
 const common = require('./webpack.common.js');
 
